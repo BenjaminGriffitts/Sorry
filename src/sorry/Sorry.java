72b6eee46f45985ff4c544817bc41e79523997b1
unknown <154002982@mvhsrop901-28.vusdnoc.vusd.k12.ca.us>
2015-10-27 10:04:55 -0700

    initial commit

---
+++ src/sorry/Sorry.java
@@ -0,0 +1,248 @@
+package sorry;
+
+
+import java.io.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Sorry extends JFrame implements Runnable {
+    static final int XBORDER = 20;
+    static final int YBORDER = 20;
+    static final int YTITLE = 30;
+    static final int WINDOW_BORDER = 8;
+    static final int WINDOW_WIDTH = 2*(WINDOW_BORDER + XBORDER) + 495;
+    static final int WINDOW_HEIGHT = YTITLE + WINDOW_BORDER + 2 * YBORDER + 525;
+    boolean animateFirstTime = true;
+    int xsize = -1;
+    int ysize = -1;
+    Image image;
+    Graphics2D g;
+
+    final int numRows = 16;
+    final int numColumns = 16;
+
+    final int EMPTY = 0;
+    
+    int board[][];
+
+    
+    static Sorry frame1;
+    public static void main(String[] args) {
+        frame1 = new Sorry();
+        frame1.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.setVisible(true);
+    }
+
+    public Sorry() {
+
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (e.BUTTON1 == e.getButton()) {
+                    //left button
+                }
+                if (e.BUTTON3 == e.getButton()) {
+                    //right button
+                    reset();
+                }
+                repaint();
+            }
+        });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseDragged(MouseEvent e) {
+        repaint();
+      }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseMoved(MouseEvent e) {
+        repaint();
+      }
+    });
+
+        addKeyListener(new KeyAdapter() {
+
+            public void keyPressed(KeyEvent e) {
+                if (e.VK_RIGHT == e.getKeyCode())
+                {
+                   
+                }
+                if (e.VK_LEFT == e.getKeyCode())
+                {
+                    
+                }
+                if (e.VK_UP == e.getKeyCode())
+                {
+                    
+                }
+                if (e.VK_DOWN == e.getKeyCode())
+                {
+                    
+                }
+                if (e.VK_D == e.getKeyCode())
+                {
+                    
+                }
+                if (e.VK_A == e.getKeyCode())
+                {
+                    
+                }
+                if (e.VK_W == e.getKeyCode())
+                {
+                   
+                }
+                if (e.VK_S == e.getKeyCode())
+                {
+                   
+                }
+                if (e.VK_E == e.getKeyCode())
+                {
+                    reset();
+                }
+
+                repaint();
+            }
+        });
+        init();
+        start();
+    }
+
+
+
+
+    Thread relaxer;
+////////////////////////////////////////////////////////////////////////////
+    public void init() {
+        requestFocus();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void destroy() {
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void paint(Graphics gOld) {
+        if (image == null || xsize != getSize().width || ysize != getSize().height) {
+            xsize = getSize().width;
+            ysize = getSize().height;
+            image = createImage(xsize, ysize);
+            g = (Graphics2D) image.getGraphics();
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+
+//fill background
+        g.setColor(Color.cyan);
+
+        g.fillRect(0, 0, xsize, ysize);
+
+        int x[] = {getX(0), getX(getWidth2()), getX(getWidth2()), getX(0), getX(0)};
+        int y[] = {getY(0), getY(0), getY(getHeight2()), getY(getHeight2()), getY(0)};
+//fill border
+        g.setColor(Color.white);
+        g.fillPolygon(x, y, 4);
+// draw border
+        g.setColor(Color.red);
+        g.drawPolyline(x, y, 5);
+
+        if (animateFirstTime) {
+            gOld.drawImage(image, 0, 0, null);
+            return;
+        }
+
+        g.setColor(Color.black);
+//horizontal lines
+        for (int zi=1;zi<numRows;zi++)
+        {
+            g.drawLine(getX(0) ,getY(0)+zi*getHeight2()/numRows ,
+            getX(getWidth2()) ,getY(0)+zi*getHeight2()/numRows );
+        }
+//vertical lines
+        for (int zi=1;zi<numColumns;zi++)
+        {
+            g.drawLine(getX(0)+zi*getWidth2()/numColumns ,getY(0) ,
+            getX(0)+zi*getWidth2()/numColumns,getY(getHeight2())  );
+        }
+
+
+        gOld.drawImage(image, 0, 0, null);
+    }
+
+
+////////////////////////////////////////////////////////////////////////////
+// needed for     implement runnable
+    public void run() {
+        while (true) {
+            animate();
+            repaint();
+            double seconds = 0.04;    //time that 1 frame takes.
+            int miliseconds = (int) (1000.0 * seconds);
+            try {
+                Thread.sleep(miliseconds);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void reset() {
+        board = new int[numRows][numColumns];
+        for (int zrow = 0;zrow < numRows;zrow++)
+        {
+            for (int zcolumn = 0;zcolumn < numColumns;zcolumn++)
+            {
+                board[zrow][zcolumn] = EMPTY;
+            }
+        }
+        
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void animate() {
+
+        if (animateFirstTime) {
+            animateFirstTime = false;
+            if (xsize != getSize().width || ysize != getSize().height) {
+                xsize = getSize().width;
+                ysize = getSize().height;
+            }
+
+            reset();
+        }
+                    
+    }
+
+////////////////////////////////////////////////////////////////////////////
+    public void start() {
+        if (relaxer == null) {
+            relaxer = new Thread(this);
+            relaxer.start();
+        }
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void stop() {
+        if (relaxer.isAlive()) {
+            relaxer.stop();
+        }
+        relaxer = null;
+    }
+/////////////////////////////////////////////////////////////////////////
+    public int getX(int x) {
+        return (x + XBORDER + WINDOW_BORDER);
+    }
+
+    public int getY(int y) {
+        return (y + YBORDER + YTITLE );
+    }
+
+    public int getYNormal(int y) {
+        return (-y + YBORDER + YTITLE + getHeight2());
+    }
+    
+    public int getWidth2() {
+        return (xsize - 2 * (XBORDER + WINDOW_BORDER));
+    }
+
+    public int getHeight2() {
+        return (ysize - 2 * YBORDER - WINDOW_BORDER - YTITLE);
+    }
+}
