6e3f4dd05dbd75b1077d93e42ad8f253aeae0810
LukeJHarvey <wxisxawesome@gmail.com>
2015-10-27 10:10:58 -0700

    Card Class

---
+++ src/sorry/Card.java
@@ -0,0 +1,12 @@
+package sorry;
+import java.util.ArrayList;
+public class Card {
+    static final int totalCards=45;
+    static ArrayList<Card> Cards = new ArrayList<Card>();
+    private int type;
+    Card()
+    {
+        
+    }
+    
+}
