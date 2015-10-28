f576bd37311a99e99a68388898a3c482b0af266a
JosephMumper <joseph.mumper@gmail.com>
2015-10-28 10:56:02 -0700

    Constructor to Piece Class

--- src/sorry/Piece.java
+++ src/sorry/Piece.java
@@ -2,5 +2,17 @@ package sorry;
 public class Piece {
     private int Column;
     private int Row;
+    private int startColumn;
+    private int startRow;
+    private Sorry.Owner team;
     
+    Piece(int _column,int _row, Sorry.Owner _team)
+    {
+        Column = _column;
+        Row = _row;
+        team = _team;
+        startColumn = Column;
+        startRow = Row;
+    }
+
 }
